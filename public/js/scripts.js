0009a7f46c7c46293ac4c2934238dcf3b72f50d7
leandro souza <leandrosr616@gmail.com>
2021-02-23 18:46:55 -0300

    configurada a inserção de dados do formulário no banco de dados

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14,3 +14,29 @@ function onOff() {
         .classList
         .toggle("addScroll");
 }
+
+function checarCampos(event) {
+    
+    const valuesToCheck = [
+        "title",
+        "image",
+        "category",
+        "description",
+        "link",
+    ]
+
+    const isEmpty = valuesToCheck.find(function(value) {
+
+        const checkIsString = typeof event.target[value].value === "string"
+        const checkIsEmpty = !event.target[value].value.trim()
+
+        if (checkIsString && checkIsEmpty) {
+            return true
+        }
+    })
+
+    if (isEmpty) {
+        event.preventDefault()
+        alert("Preencha todos os campos")
+    }
+}
